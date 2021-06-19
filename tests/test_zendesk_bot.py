164b227d8a808a330ff8569171666efeb9ef6f32
Alex Rong <alex.rong@brighte.com.au>
2021-06-19 19:12:57 +1000

    Add Database, Table and Index class; Add corresponding test cases;

--- tests/test_zendesk_bot.py
+++ tests/test_zendesk_bot.py
@@ -1,19 +1,78 @@
 import pytest
 from zendesk.processor import Processor
-from zendesk.db import Database
+from zendesk.db import Database, Table, Index, TableNotExistsException
+
+from typing import Dict
 
 @pytest.fixture()
 def db():
     db = Database()
+    db.load()
     return db
 
-def test_search_field_by_value(db):
+# def test_search_field_by_value(db):
+#
+#     process = Processor()
+#
+#     item = '1'
+#     field = '_id'
+#     value = '71'
+#
+#     result = process.search(db, item, field, value)
+#     assert len(result) == 1
 
-    process = Processor()
 
-    item = '1'
-    field = '_id'
-    value = '71'
+class TestProcessor:
 
-    result = process.search(db, item, field, value)
-    assert len(result) == 1
+    def test_initialise_processor(self):
+        processor = Processor()
+        assert isinstance(processor, Processor)
+
+
+class TestDatabase:
+
+    def test_load(self, db):
+        for table_name, collections in db.collections.items():
+            assert isinstance(table_name, str)
+            assert isinstance(collections, Table)
+
+    def test__exception(self, db):
+        with pytest.raises(TableNotExistsException):
+            db.search('table_not_exists', 'field', 'value')
+
+
+class TestTable:
+
+    def test_build_table(self, db):
+        users = db.collections.get('users')
+
+    def test_build_index(self, db):
+        users_indexes = db.collections.get('users').indexes
+        for key, index in users_indexes.items():
+            assert isinstance(key, str)
+            assert isinstance(index, Index)
+
+    def test__search_field_index(self, db):
+        users_table = db.collections.get('users')
+        res = users_table._search_by_index('_id', 1)
+        assert isinstance(res, list)
+
+    def test__search_field_no_index(self, db):
+        users_table = db.collections.get('users')
+        res = users_table._search_by_index('url', "http://initech.zendesk.com/api/v2/users/1.json")
+        assert res == None
+
+
+class TestIndex:
+
+    def test_initialisation_index(self):
+        index = Index('_id')
+        assert index.references == {}
+
+    def test_search_index(self):
+        index = Index('_id')
+        reference = index.references.get(71)
+        assert reference == None
+
+
+
--- zendesk/db.py
+++ zendesk/db.py
@@ -1,25 +1,134 @@
 import json
 import os.path
-from typing import Dict
+from typing import Dict, List, Iterable, Any, Optional, Union
+from collections import defaultdict
+from dataclasses import dataclass, field
 
+from .utilties import get_logger
 from .model import Organizations, Tickets, Users
+
+logger = get_logger(__name__)
+
+resources = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'resources')
 
 class TableNotExistsException(Exception):
     pass
 
+class ColumnNotExistsException(Exception):
+    pass
+
+
+@dataclass
+class Index():
+    '''
+    Index class
+    Example
+    name: _id
+    references: {71: [1], 52: [3]}
+    '''
+    name: str
+    references: Dict[str, List] = field(default_factory=lambda: defaultdict(list))
+
+    def search(self, key: Union[int, str]) -> Optional[List]:
+        return self.references.get(key)
+
+
+
+@dataclass
+class Table():
+    name: str
+    # TODO: design primary key and foreign key structure
+    primary_key: str = ""
+    foreign_key: str = ""
+    records: List[Dict[Any, Any]] = field(default_factory=list)
+    indexes: Dict[str, Index] = field(default_factory=lambda: defaultdict(Index))
+
+    def build_index(self):
+        '''
+        Construct index data structure
+        '''
+        idx_col = self.primary_key
+        idx = Index(idx_col)
+        for i, record in enumerate(self.records):
+            key = record.get(idx_col)
+            idx.references[key].append(i)
+
+        self.indexes[idx_col] = idx
+
+
+    def _search_by_index(self, field, value) -> Optional[List[int]]:
+        '''
+        Search by field value and return the index of occurrence
+        '''
+
+        if index_record := self.indexes.get(field):
+            reference = index_record.search(value)
+            return reference
+
+    def _sequential_search(self, field, value) -> List:
+        res = []
+        for record in self.records:
+            if find := record.get(field):
+                if find == value:
+                    res.append(record)
+        return res
+
+
+    def search(self, field, value) -> List:
+        # TODO: sentinel check field exists
+        indexes = self._search_by_index(field, value)
+
+        if indexes:
+            res = [self.records[i] for i in indexes]
+            return res
+        else:
+            res = self._sequential_search(field, value)
+            return res
+
+
+
 class Database():
     def __init__(self):
-        self.collections = {}
+        self.name: str = 'zendesk'
+        self.collections: Dict[str, Table] = {}
 
-    def load_data(self) -> Dict:
+    def load(self, tables: List[str] = ['tickets', 'organizations', 'users']) -> None:
+        '''
+        Load files matching names in the give @tables parameters
+        '''
 
-        tables = ['tickets', 'users', 'organizations']
-        dir = os.path.dirname(os.path.realpath(__file__))
-        for table in tables:
-            filename = os.path.join(dir, type + '.json')
-            with open(filename, 'r') as f:
-                data = json.load(f)
-                self.collections[table] = data
+        index_keys = {
+            'users': '_id',
+            'organizations': '_id',
+            'tickets': 'submitter_id'
+
+        }
+
+        for table_name in tables:
+            filename = os.path.join(resources, table_name + '.json')
+            logger.info(f'Loading {filename}')
+
+            idx = index_keys.get(table_name)
+
+            table = Table(name=table_name, primary_key=idx)
+
+            try:
+                with open(filename, 'r') as f:
+                    source = json.load(f)
+                    table.records = source
+                    self.collections[table_name] = table
+                    table.build_index()
+            except FileNotFoundError:
+                logger.error(f'{filename} not exists')
+
+    def search(self, entity, field, value):
+        table = self.collections.get(entity)
+        if table:
+            res = table.search(field, value)
+            return res
+        else:
+            raise TableNotExistsException
+
 
 
 
